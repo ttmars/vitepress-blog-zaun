faf1dfd521b168b5db6a5e2a5e84ac5d8fee049f
liwen <liwen@gzntkj.com>
2023-12-28 17:30:47 +0800

    将评论插件gitalk切换为giscus

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,6 +4,9 @@ import Tags from "./components/Tags.vue";
 import MyLayout from "./components/MyLayout.vue";
 import "./custom.css";
 
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
+
 export default {
   ...DefaultTheme,
   Layout: MyLayout,
@@ -14,4 +17,31 @@ export default {
     app.component("Archives", Archives);
     app.component("Tags", Tags);
   },
+  setup() {
+    // 获取前言和路由
+    const { frontmatter } = useData();
+    const route = useRoute();
+    
+    // 评论组件 - https://giscus.app/
+    giscusTalk({
+        repo: 'ttmars/vitepress-blog-zaun',
+        repoId: 'R_kgDOK-ZqPA',
+        category: 'Announcements', // 默认: `General`
+        categoryId: 'DIC_kwDOK-ZqPM4CcDMK',
+        mapping: 'pathname', // 默认: `pathname`
+        inputPosition: 'top', // 默认: `top`
+        lang: 'zh-CN', // 默认: `zh-CN`
+        lightTheme: 'light', // 默认: `light`
+        darkTheme: 'transparent_dark', // 默认: `transparent_dark`
+        // ...
+    }, {
+        frontmatter, route
+    },
+        // 是否全部页面启动评论区。
+        // 默认为 true，表示启用，此参数可忽略；
+        // 如果为 false，表示不启用。
+        // 可以在页面使用 `comment: true` 前言单独启用
+        true
+    );
+}
 };
